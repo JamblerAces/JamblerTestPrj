7dc84ab2c3be941a53b87db29cb8dd5697da34b0
Jambler <@>
2014-11-25 02:02:05 +0200

    Starting Entities

---
+++ src/main/java/domain/OrderItem.java
@@ -0,0 +1,125 @@
+package domain;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orderItem")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String itemUrl;
+    private Shop shop;
+    private String color;
+    private String size;
+    private int amount;
+    private double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(String itemUrl, Shop shop, double price, int amount) {
+        this.itemUrl = itemUrl;
+        this.shop = shop;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getItemUrl() {
+        return itemUrl;
+    }
+
+    public void setItemUrl(String itemUrl) {
+        this.itemUrl = itemUrl;
+    }
+
+    public Shop getShop() {
+        return shop;
+    }
+
+    public void setShop(Shop shop) {
+        this.shop = shop;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem that = (OrderItem) o;
+
+        return Objects.equal(this.itemUrl, that.itemUrl) &&
+                Objects.equal(this.shop, that.shop) &&
+                Objects.equal(this.color, that.color) &&
+                Objects.equal(this.size, that.size) &&
+                Objects.equal(this.amount, that.amount) &&
+                Objects.equal(this.price, that.price) &&
+                Objects.equal(this.id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(itemUrl, shop, color, size, amount, price,
+                id);
+    }
+
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("itemUrl", itemUrl)
+                .add("shop", shop)
+                .add("color", color)
+                .add("size", size)
+                .add("amount", amount)
+                .add("price", price)
+                .add("id", id).
+                        omitNullValues()
+                .toString();
+    }
+}
--- src/main/java/domain/Shop.java
+++ src/main/java/domain/Shop.java
@@ -1,15 +1,77 @@
 package domain;
 
-/**
- * Created by Jambler on 23.11.2014.
- */
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "shop")
 public class Shop {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String name;
     private String mainDomen;
+
+    public Shop() {
+    }
 
     public Shop(String name, String mainDomen) {
         this.name = name;
         this.mainDomen = mainDomen;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMainDomen() {
+        return mainDomen;
+    }
+
+    public void setMainDomen(String mainDomen) {
+        this.mainDomen = mainDomen;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Shop that = (Shop) o;
+
+        return Objects.equal(this.name, that.name) &&
+                Objects.equal(this.mainDomen, that.mainDomen) &&
+                Objects.equal(this.id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name, mainDomen, id);
+    }
+
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("name", name)
+                .add("mainDomen", mainDomen)
+                .add("id", id)
+                .toString();
     }
 }
