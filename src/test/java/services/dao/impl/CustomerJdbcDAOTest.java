06d22c65dbe7019639dd3930438c848ec4fc9782
Stanislav Koval <stanislav.koval@live.com>
2014-12-02 18:29:42 +0200

    HibernateCustomerDao #BFM-9 work Development 1d

--- src/test/java/services/dao/impl/CustomerJdbcDAOTest.java
+++ src/test/java/services/dao/impl/CustomerJdbcDAOTest.java
@@ -37,7 +37,8 @@ public class CustomerJdbcDAOTest {
     public void testCreate() throws Exception {
         Customer newCus = new Customer("testNew");
         dao.save(newCus);
-        /*testing commit message*/
+        /*testing commit message
+        * and again*/
         List<Customer> customers = dao.findAll();
         Collection<Customer> customersFiltered = Collections2.filter(customers, new CustomerPredicateByName());
         assertTrue(customersFiltered.size() > 0);
