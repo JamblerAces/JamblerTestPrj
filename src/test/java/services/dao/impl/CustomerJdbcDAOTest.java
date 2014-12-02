200b32e39e06c1c9b18e09a6c36f91a381ade24a
Stanislav Koval <stanislav.koval@live.com>
2014-12-02 18:45:48 +0200

    HibernateCustomerDao #BFM-8 fixed

--- src/test/java/services/dao/impl/CustomerJdbcDAOTest.java
+++ src/test/java/services/dao/impl/CustomerJdbcDAOTest.java
@@ -38,7 +38,7 @@ public class CustomerJdbcDAOTest {
         Customer newCus = new Customer("testNew");
         dao.save(newCus);
         /*testing commit message
-        * and fgfg */
+        * and fgfg gfgh ggh gh gh */
         List<Customer> customers = dao.findAll();
         Collection<Customer> customersFiltered = Collections2.filter(customers, new CustomerPredicateByName());
         assertTrue(customersFiltered.size() > 0);
